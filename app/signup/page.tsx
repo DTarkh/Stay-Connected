27736a8dc57e45e899b00cb0a22175cdb1c9dfaa
juglika <lashajugeli96@gmail.com>
2024-12-06 21:44:45 +0400

    feat: add sign up page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,14 +2,11 @@
 import { CiLock } from "react-icons/ci";
 import { CiMail } from "react-icons/ci";
 import { FormEvent, useState } from "react";
-import Link from "next/link";
+import { CgProfile } from "react-icons/cg";
 import { GoEyeClosed } from "react-icons/go";
 import { GoEye } from "react-icons/go";
 
-// import { FormEvent } from "react";
-
-// import {getSession} from '@/lib'
-const Page = () => {
+const Signup = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
@@ -23,8 +20,28 @@ const Page = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
-        <h2 className="text-2xl font-bold text-black mb-6">Login</h2>
+        <h2 className="text-2xl font-bold text-black mb-6">Sign Up</h2>
         <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="username" className="block text-gray-500 mb-2">
+              Full Name
+            </label>
+            <div className="relative flex items-center">
+              <CgProfile
+                size="25"
+                color="gray"
+                className="absolute left-3 top-1/2 transform -translate-y-1/2"
+              />
+              <input
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Enter your Name"
+                required
+                className="pl-10 w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500 text-black"
+              />
+            </div>
+          </div>
           <div className="mb-4">
             <label htmlFor="username" className="block text-gray-500 mb-2">
               Email
@@ -43,6 +60,41 @@ const Page = () => {
                 required
                 className="pl-10 w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500 text-black"
               />
+            </div>
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-gray-500 mb-2">
+              Enter Password
+            </label>
+            <div className="relative flex items-center">
+              <CiLock
+                size="25"
+                color="gray"
+                className="absolute left-3 top-1/2 transform -translate-y-1/2"
+              />
+              <input
+                type={passwordVisible ? "text" : "password"}
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+                required
+                className="pl-10 w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500 text-black"
+              />
+              {passwordVisible ? (
+                <GoEye
+                  onClick={() => setPasswordVisible(false)}
+                  size="20"
+                  color="grey"
+                  className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                />
+              ) : (
+                <GoEyeClosed
+                  onClick={() => setPasswordVisible(true)}
+                  size="20"
+                  color="grey"
+                  className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                />
+              )}
             </div>
           </div>
           <div className="mb-6">
@@ -80,19 +132,12 @@ const Page = () => {
               )}
             </div>
           </div>
-          <div className="flex items-center justify-between mb-4">
-            <span className="text-gray-500 text-sm">New To StayConnected?</span>
-            <Link href={"/signup"}>
-              <span className="cursor-pointer text-gray-500 text-base font-bold">
-                Sign Up
-              </span>
-            </Link>
-          </div>
+
           <button
             type="submit"
             className="w-full bg-purple-600 text-white font-bold py-2 px-4 rounded hover:bg-purple-00 hover:text-gray-100 transition-colors"
           >
-            Login
+            Sign Up
           </button>
         </form>
       </div>
@@ -100,4 +145,4 @@ const Page = () => {
   );
 };
 
-export default Page;
+export default Signup;
