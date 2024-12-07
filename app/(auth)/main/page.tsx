96d773b84d523c6ed923a2fc55601ecb3ad03a5f
FleecyBear <nikusha.kk1990@gmail.com>
2024-12-07 12:44:20 +0400

    Updated Main Page
    Fetching with apiFetcher now

--- app/(auth)/main/page.tsx
+++ app/(auth)/main/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Questions, { QuestionData } from "../../Components/Question";
 import Link from "next/link";
 import Ratings from "../../Components/Ratings";
+import { apiFetcher, API_ROUTES } from "@/app/utils/apiClient";  
 
 interface Props {
   searchParams: { search?: string; tag?: string };
@@ -10,25 +11,22 @@ interface Props {
 const Main = async ({ searchParams }: Props) => {
   const search = searchParams.search || "";
   const tag = searchParams.tag || "";
-  let url = "https://nunu29.pythonanywhere.com/questions/";
+  let url = API_ROUTES.submitAnswer; 
   const queryParams: string[] = [];
 
   if (search) queryParams.push(`search=${search}`);
   if (tag) queryParams.push(`tags=${tag}`);
   if (queryParams.length > 0) {
-    url += `?${queryParams.join("&")}`;
+    url += `?${queryParams.join("&")}`;  
   }
   console.log("Fetching from URL:", url);
 
   let questions: QuestionData[] = [];
   try {
-    const response = await fetch(url, { cache: "no-store" });
-    console.log("Fetch response status:", response.status);
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    questions = await response.json();
+    questions = await apiFetcher<QuestionData[]>("submitAnswer", { 
+      method: "GET", 
+    });
+    console.log("Fetched questions:", questions);
   } catch (err) {
     console.error("Error fetching questions:", err);
   }
@@ -48,7 +46,7 @@ const Main = async ({ searchParams }: Props) => {
           ))
         )}
       </div>
-      <div className="border-2 border-current max-lg:hidden  mt-4 rounded-xl flex flex-col items-center p-4 overflow-auto">
+      <div className="border-2 border-current max-lg:hidden mt-4 rounded-xl flex flex-col items-center p-4 overflow-auto">
         <Ratings />
       </div>
     </main>
