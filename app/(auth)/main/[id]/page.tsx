40b45f5cfc2ceff55b7b1e9b108c1a8d5dcd54f3
FleecyBear <nikusha.kk1990@gmail.com>
2024-12-07 11:47:33 +0400

    Fixed: Build errors

--- app/(auth)/main/[id]/page.tsx
+++ app/(auth)/main/[id]/page.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { GetServerSideProps } from "next";
 import { cookies } from "next/headers"; 
 import { fetchQuestion } from "@/app/Components/forDetailed/fetchQuestion";
 import ErrorPage from "@/app/Components/forDetailed/ErrorPage";
 import QuestionDetails from "@/app/Components/forDetailed/QuestionDetails";
 import AnswerList from "@/app/Components/forDetailed/AnswerList";
 import AddAnswerForm from "@/app/Components/forDetailed/AddAnswerForm";
+
+interface Answer {
+  id: number;
+  user: string;
+  body: string;
+  created_at: string;
+  like_count: number;
+  dislike_count: number;
+}
 
 interface Question {
   id: number;
@@ -16,20 +24,19 @@ interface Question {
   like_count: number;
   dislike_count: number;
   created_at: string;
-  answers: any[];
+  answers: Answer[];
   number_of_answers: number;
 }
 
 interface Props {
   params: { id: string };
-  accessToken: string; 
 }
 
 const QuestionDetailPage = async ({ params }: Props) => {
   const { id } = params;
 
   const cookieStore = cookies();
-  const accessToken = cookieStore.get("accessToken")?.value || ""; 
+  const accessToken = cookieStore.get("accessToken")?.value || "";
 
   let question: Question | null = null;
   let error: string | null = null;
@@ -56,11 +63,8 @@ const QuestionDetailPage = async ({ params }: Props) => {
   return (
     <main className="flex flex-col min-h-screen justify-center gap-5 bg-blue-400 p-4 mx-12">
       <QuestionDetails question={question} />
-      <AnswerList answers={question.answers} number_of_answers={question.number_of_answers} /> 
-      <AddAnswerForm
-        questionId={question.id.toString()}
-        accessToken={accessToken} 
-      />
+      <AnswerList answers={question.answers} number_of_answers={question.number_of_answers} />
+      <AddAnswerForm questionId={question.id.toString()} accessToken={accessToken} />
     </main>
   );
 };
