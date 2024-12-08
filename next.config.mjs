459d32cba4cfd2a4f1e4f7209b4ab01fa7c80aa4
FleecyBear <nikusha.kk1990@gmail.com>
2024-12-08 16:09:49 +0400

    Chores
    1. Removed unused tsx file;
    2. Updated next.config.mjs for image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { images: { domains: ['img.freepik.com'], }, };
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['res.cloudinary.com', 'img.freepik.com'], 
+    },
+  };
+  
+  export default nextConfig;
+  
